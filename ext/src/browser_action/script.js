9c99d34ece99cc7dec5664f9b7c70a62ee202b8d
Belton Zhong <Belton Zhong>
2017-06-07 15:12:56 -0400

    Added code from Webcam Screenshot

--- ext/src/browser_action/script.js
+++ ext/src/browser_action/script.js
@@ -3,13 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
 			var video = document.getElementById('video');
 
 			// Get access to the camera!
-			if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 			    // Not adding `{ audio: true }` since we only want video now
-			    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-			        video.src = window.URL.createObjectURL(stream);
-			        video.play();
-			    });
-			}
+      var success = function (stream) {
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+      }.bind(this);
+
+      var failure = function (stream) {
+        alert("ERROR");
+      }.bind(this);
+
+			navigator.getUserMedia({ video: true, audio: false }, success, failure);
     /*
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
