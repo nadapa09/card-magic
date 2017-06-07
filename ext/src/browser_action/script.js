38d87f82b5c59debacb89e0eac54e4feb2627435
Belton Zhong <Belton Zhong>
2017-06-07 13:53:19 -0400

    Fixed JS error

--- ext/src/browser_action/script.js
+++ ext/src/browser_action/script.js
@@ -1,5 +1,5 @@
 			// Grab elements, create settings, etc.
-		function go() {
+document.addEventListener('DOMContentLoaded', function() {
 			var video = document.getElementById('video');
 
 			// Get access to the camera!
@@ -10,4 +10,24 @@
 			        video.play();
 			    });
 			}
-		}
+    /*
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'http://gtmetrix.com/analyze.html?bm';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'hidden';
+      i.name = 'url';
+      i.value = tab.url;
+      f.appendChild(i);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+    */
+}, false);
